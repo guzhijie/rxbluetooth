3630e61253b28c48edcd4b97dd62f4784bfbbb7f
jack.gu <guzhijie1981@163.com>
2019-10-25 22:12:45 +0800

    2019年10月25日

---
+++ app/src/main/java/com/jack/test/Adapter.java
@@ -0,0 +1,98 @@
+package com.jack.test;
+
+import android.annotation.SuppressLint;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.inuker.bluetooth.library.search.SearchResult;
+import com.jack.rx.bluetooth.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * 描述:
+ *
+ * @author :jack.gu
+ * @since : 2019/10/25 0025
+ */
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+    public final List<SearchResult> m_searchResults = new ArrayList<>();
+    private OnItemClickListener m_onItemClickListener;
+    private OnItemLongClickListener m_onItemLongClickListener;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.ble_item_adapter_view, viewGroup, false);
+        return new ViewHolder(itemView);
+    }
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        TextView textView = viewHolder.item;
+        SearchResult searchResult = m_searchResults.get(i);
+        String addr = searchResult.device.getAddress();
+        String name = searchResult.device.getName();
+        int rssi = searchResult.rssi;
+        textView.setText(String.format("蓝牙地址:%s, 名字:%s, 信号强度:%d", addr, name, rssi));
+    }
+
+    @Override
+    public int getItemCount() {
+        return m_searchResults.size();
+    }
+
+    public Adapter setOnItemClickListener(OnItemClickListener l) {
+        this.m_onItemClickListener = l;
+        return this;
+    }
+
+    public Adapter setOnItemLongClickListener(OnItemLongClickListener l) {
+        this.m_onItemLongClickListener = l;
+        return this;
+    }
+
+    interface OnItemClickListener {
+        void OnItemClick(View view, int pos, Object o);
+    }
+
+
+    interface OnItemLongClickListener {
+        boolean OnItemLongClick(View view, int pos, Object o);
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.item)
+        TextView item;
+        private Adapter m_adapter;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            m_adapter = Adapter.this;
+            item.setOnClickListener(v -> {
+                if (m_adapter.m_onItemClickListener != null) {
+                    int pos = getAdapterPosition();
+                    m_adapter.m_onItemClickListener.OnItemClick(v, pos, m_adapter.m_searchResults.get(pos));
+                }
+            });
+            item.setOnLongClickListener(v -> {
+                if (m_adapter.m_onItemLongClickListener != null) {
+                    int pos = getAdapterPosition();
+                    return m_adapter.m_onItemLongClickListener.OnItemLongClick(item, pos, m_adapter.m_searchResults.get(pos));
+                }
+                return true;
+            });
+        }
+
+    }
+}
--- app/src/main/java/com/jack/test/MainActivity.java
+++ app/src/main/java/com/jack/test/MainActivity.java
@@ -2,14 +2,9 @@ package com.jack.test;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.inuker.bluetooth.library.search.SearchRequest;
 import com.inuker.bluetooth.library.search.SearchResult;
@@ -17,20 +12,18 @@ import com.jack.rx.bluetooth.R;
 import com.jack.rx.bluetooth.RxBluetooth;
 import com.jack.test.js100.JS100BluetoothHolder;
 import com.jack.test.js100.JS100Param;
+import com.jack.test.js100.JS100SampleFrequency;
+import com.jack.test.js100.JS100SamplePoint;
+import com.jack.test.js100.JS100SampleType;
 import com.jack.test.js100.JS100SensorData;
 import com.jack.test.zc1000.ZC1000BluetoothHolder;
 import com.jack.test.zc1000.ZC1000SensorData;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.OnItemClick;
 import butterknife.Unbinder;
 
 @SuppressWarnings("ResultOfMethodCallIgnored")
@@ -40,104 +33,64 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView bleList;
     @BindView(R.id.search_ble)
     Button searchBle;
-    private Unbinder unbinder;
-    private List<SearchResult> searchResults = new ArrayList<>();
+    private Unbinder m_unbinder;
 
-    @SuppressWarnings("unchecked")
     @SuppressLint("CheckResult")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        unbinder = ButterKnife.bind(this);
-        final String mac = "";
-        RxBluetooth.getInstance()
-                .connect(mac, new SensorBluetoothHolderFactory())
-                .map(bluetoothHolder -> (SensorBluetoothHolder) bluetoothHolder)
-                .concatMap(sensorBluetoothHolder -> {
-                    Type type = sensorBluetoothHolder.getType();
-                    if (type instanceof JS100SensorData) {
-                        JS100BluetoothHolder js100BluetoothHolder = (JS100BluetoothHolder) sensorBluetoothHolder;
-                        return js100BluetoothHolder.sensorObservable(new JS100Param());
-                    } else {
-                        ZC1000BluetoothHolder zc1000BluetoothHolder = (ZC1000BluetoothHolder) sensorBluetoothHolder;
-                        return zc1000BluetoothHolder.sensorObservable(null);
-                    }
-                })
-                .subscribe(sensorData -> {
-                    if (sensorData instanceof JS100SensorData) {
-                        JS100SensorData js100SensorData = (JS100SensorData) sensorData;
-                        Float vibrate = js100SensorData.getVibrate();
-                        Float speed = js100SensorData.getSpeed();
-                        Float temperature = js100SensorData.getTemperature();
-                    } else if (sensorData instanceof ZC1000SensorData) {
-                        ZC1000SensorData zc1000SensorData = (ZC1000SensorData) sensorData;
-                        String temperature = zc1000SensorData.getTemperature();
-                        String vibrate = zc1000SensorData.getVibrate();
-                        String rfid = zc1000SensorData.getRFID();
-                    }
-                });
-    }
-
-
-    @SuppressLint("CheckResult")
-    @OnClick(value = R.id.search_ble)
-    void searchBleOnClick() {
-        RxBluetooth.getInstance()
+        m_unbinder = ButterKnife.bind(this);
+        Adapter adapter = new Adapter();
+        adapter.setOnItemClickListener((view, pos, o) -> {
+            SearchResult searchResult = (SearchResult) o;
+            RxBluetooth.getInstance().connect(searchResult.device.getAddress(), new SensorBluetoothHolderFactory())
+                    .map(bluetoothHolder -> (SensorBluetoothHolder) bluetoothHolder)
+                    .concatMap(sensorBluetoothHolder -> {
+                        Type type = sensorBluetoothHolder.getType();
+                        if (type instanceof JS100SensorData) {
+                            JS100BluetoothHolder js100BluetoothHolder = (JS100BluetoothHolder) sensorBluetoothHolder;
+                            return js100BluetoothHolder.sensorObservable(new JS100Param()
+                                    .setType(JS100SampleType.Temperature_Vibrate)
+                                    .setFrequency(JS100SampleFrequency.Freq_1kHz)
+                                    .setFactor(100)
+                                    .setPoint(JS100SamplePoint.Point_512)                            );
+                        } else {
+                            ZC1000BluetoothHolder zc1000BluetoothHolder = (ZC1000BluetoothHolder) sensorBluetoothHolder;
+                            return zc1000BluetoothHolder.sensorObservable(null);
+                        }
+                    })
+                    .subscribe(sensorData -> {
+                        if (sensorData instanceof JS100SensorData) {
+                            JS100SensorData js100SensorData = (JS100SensorData) sensorData;
+                            Float vibrate = js100SensorData.getVibrate();
+                            Float speed = js100SensorData.getSpeed();
+                            Float temperature = js100SensorData.getTemperature();
+                        } else if (sensorData instanceof ZC1000SensorData) {
+                            ZC1000SensorData zc1000SensorData = (ZC1000SensorData) sensorData;
+                            String temperature = zc1000SensorData.getTemperature();
+                            String vibrate = zc1000SensorData.getVibrate();
+                            String rfid = zc1000SensorData.getRFID();
+                        }
+                    });
+        });
+        bleList.setAdapter(adapter);
+        searchBle.setOnClickListener(v -> RxBluetooth.getInstance()
                 .search(new SearchRequest.Builder()
-                        .searchBluetoothLeDevice(3000, 3)   // 先扫BLE设备3次，每次3s
-                        .searchBluetoothClassicDevice(5000) // 再扫经典蓝牙5s
-                        .searchBluetoothLeDevice(2000)      // 再扫BLE设备2s
+                        .searchBluetoothLeDevice(3000, 3)
+                        .searchBluetoothClassicDevice(5000)
+                        .searchBluetoothLeDevice(2000)
                         .build(), 10, TimeUnit.SECONDS)
                 .subscribe(searchResult -> {
-                    this.searchResults.add(searchResult);
-                    Objects.requireNonNull(this.bleList.getAdapter()).notifyDataSetChanged();
-                });
+                    adapter.m_searchResults.add(searchResult);
+                    adapter.notifyItemInserted(adapter.m_searchResults.size());
+                }));
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unbinder.unbind();
+        m_unbinder.unbind();
     }
 
-    class Adapter extends RecyclerView.Adapter {
-
-        @NonNull
-        @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-            View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.ble_item_adapter_view, viewGroup, false);
-            return new ViewHolder(itemView);
-        }
-
-        @SuppressLint("DefaultLocale")
-        @Override
-        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
-            TextView textView = (TextView) viewHolder.itemView;
-            SearchResult searchResult = searchResults.get(i);
-            String addr = searchResult.device.getAddress();
-            String name = searchResult.device.getName();
-            int rssi = searchResult.rssi;
-            textView.setText(String.format("蓝牙设备: %s, 地址: %s, 信号强度:%d", addr, name, rssi));
-        }
-
-        @Override
-        public int getItemCount() {
-            return searchResults.size();
-        }
-
-        class ViewHolder extends RecyclerView.ViewHolder {
-            @BindView(R.id.item)
-            TextView item;
-
-            ViewHolder(@NonNull View itemView) {
-                super(itemView);
-                ButterKnife.bind(this, itemView);
-            }
-
-            @OnClick(value = R.id.item)
-            void itemViewOnClick() {
-            }
-        }
-    }
 }
